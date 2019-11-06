1002aa5852e00104bba45ad772923ebc8dafe003
atrindade <atrindade@umov.me>
2019-11-06 18:03:34 -0300

    Adiciona novo cenário para feature 2 garantindo o mesmo tempo de execução

--- src/test/java/com/test/f2/Feature2Steps.java
+++ src/test/java/com/test/f2/Feature2Steps.java
@@ -7,23 +7,23 @@ import cucumber.api.java.pt.Quando;
 public class Feature2Steps {
 	long SLEEP=1000L;
 	
-	@Dado("que a análise esteja sendo preparada")
-	public void que_a_analise_esteja_sendo_preparada() throws InterruptedException {
+	@Dado("que a análise esteja sendo preparada {int}")
+	public void que_a_analise_esteja_sendo_preparada(int scenario) throws InterruptedException {
 		Thread.sleep(SLEEP);
-        System.out.format("f2 1 - cenário:1 - Thread ID: %2d\n", Thread.currentThread().getId());
+        System.out.format("f2 1 - cenário: "+scenario+" - Thread ID: %2d\n", Thread.currentThread().getId());
 	}
 
-	@Quando("eu realizo alguma ação")
-	public void eu_realizo_alguma_acao() throws InterruptedException {
+	@Quando("eu realizo alguma ação {int}")
+	public void eu_realizo_alguma_acao(int scenario) throws InterruptedException {
 		Thread.sleep(SLEEP);
-		System.out.format("f2 2 - cenário:1 - Thread ID: %2d\n", Thread.currentThread().getId());
+		System.out.format("f2 2 - cenário: "+scenario+" - Thread ID: %2d\n", Thread.currentThread().getId());
 	}
 
 
-	@Entao("os dados são listados")
-	public void os_dados_são_listados() throws InterruptedException {
+	@Entao("os dados são listados {int}")
+	public void os_dados_são_listados(int scenario) throws InterruptedException {
 		Thread.sleep(SLEEP);
-		System.out.format("f2 3 - cenário:1 - Thread ID: %2d\n", Thread.currentThread().getId());
+		System.out.format("f2 3 - cenário: "+scenario+" - Thread ID: %2d\n", Thread.currentThread().getId());
 	}
 
 }
--- src/test/resources/test/feature2.feature
+++ src/test/resources/test/feature2.feature
@@ -2,8 +2,12 @@
 
 Funcionalidade: Feature 2
 
-	Cenário: Cenário 2
-		Dado que a análise esteja sendo preparada
-		Quando eu realizo alguma ação
-		Entao os dados são listados
+	Cenário: Cenário 1
+		Dado que a análise esteja sendo preparada 1
+		Quando eu realizo alguma ação 1
+		Entao os dados são listados 1
 
+	Cenário: Cenário 2
+		Dado que a análise esteja sendo preparada 2
+		Quando eu realizo alguma ação 2
+		Entao os dados são listados 2
